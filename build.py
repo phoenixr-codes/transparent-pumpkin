9fc72782c81659975b047575896a65f38518c7b5
phoenixr-codes <phoenixr41414141@gmail.com>
2023-04-04 13:59:55 +0200

    Create python build script

---
+++ build.py
@@ -0,0 +1,25 @@
+#/usr/bin/env python3
+
+import json
+import pathlib
+import zipfile
+
+NAME = "transparent-pumpkin"
+
+src = pathlib.Path("src")
+
+with (src / "manifest.json").open("r") as f:
+    data = json.load(f)
+    version = ".".join(map(str, data["header"]["version"]))
+
+print(f"building v{version} ...")
+
+with zipfile.ZipFile(f"build/{NAME}-v{version}.mcpack", "w") as archive:
+    for path in src.rglob("*"):
+        archive.write(
+            path,
+            arcname=path.relative_to(src)
+        )
+
+#with zipfile.ZipFile(f"build/{NAME}-v{version}.mcpack", "r") as archive:
+#    archive.printdir()
--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd src &&
-zip -r transparent-pumpkin.mcpack * &&
-mv transparent-pumpkin.mcpack .. &&
-cd ..
